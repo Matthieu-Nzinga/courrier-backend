d6705027fddb845b7e6e24f44ff97935faffc85e
Matthieu Nzinga <matthieunzinga@gmail.com>
2025-11-18 12:42:52 +0100

    chore: add express app configuration (helmet, cors, morgan, json)

--- src/app.js
+++ src/app.js
@@ -1,24 +1,19 @@
-const express = require('express');
-const cors = require('cors');
-const helmet = require('helmet');
-const morgan = require('morgan');
-const routes = require('./routes');
-require('dotenv').config();
-
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const routes = require("./routes");
+require("dotenv").config();
 
 const app = express();
-
 
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
+app.use("/api", routes);
 
-app.use('/api', routes);
-
-
-app.get('/', (req, res) => res.json({ ok: true, service: 'courrier-backend' }));
-
+app.get("/", (req, res) => res.json({ ok: true, service: "courrier-backend" }));
 
 module.exports = app;
