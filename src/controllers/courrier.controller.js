f8b2337fbf40fdc05d311207bf38f327b97176a1
Matthieu Nzinga <matthieunzinga@gmail.com>
2025-11-18 17:19:53 +0100

    feat(courrier): add courrier contollers

---
+++ src/controllers/courrier.controller.js
@@ -0,0 +1,37 @@
+const courrierService = require("../services/courrier.service");
+
+exports.getCourriers = async (req, res) => {
+  try {
+    const data = await courrierService.findAll();
+    res.json(data);
+  } catch (err) {
+    res.status(500).json({ message: "Erreur serveur", err });
+  }
+};
+
+exports.getCourrierById = async (req, res) => {
+  try {
+    const data = await courrierService.findById(req.params.id);
+    if (!data) return res.status(404).json({ message: "Courrier introuvable" });
+    res.json(data);
+  } catch (err) {
+    res.status(500).json({ message: "Erreur serveur", err });
+  }
+};
+
+exports.createCourrier = async (req, res) => {
+  try {
+    const data = await courrierService.create({
+      origine: req.body.origine,
+      objet: req.body.objet,
+      date_signature: req.body.date_signature,
+      fichier_joint: req.body.fichier_joint, 
+      typeId: req.body.typeId,
+      destUserId: req.body.destUserId,
+      creatorId: req.user.id,
+    });
+    res.status(201).json(data);
+  } catch (err) {
+    res.status(400).json({ message: "Erreur lors de la création", err });
+  }
+};
