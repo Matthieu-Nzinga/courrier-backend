c9fe39a21c09c7af4122cd81926ae1d1a697d785
ghislain pango <pangolongomo@gmail.com>
2025-11-22 12:51:11 +0100

    updated setting

--- src/app.js
+++ src/app.js
@@ -7,13 +7,34 @@ const setupSwagger = require("./utils/swagger");
 const path = require("path");
 require("dotenv").config();
 
+const FRONTEND_ORIGIN = process.env.FRONTEND_ORIGIN || "http://localhost:5173";
 const app = express();
 
 app.use(helmet());
 app.use(cors());
+
+app.use(
+  cors({
+    origin: FRONTEND_ORIGIN,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 app.use(morgan("dev"));
-app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
+
+app.use("/uploads", (req, res, next) => {
+  res.removeHeader("X-Frame-Options");
+  res.setHeader(
+    "Content-Security-Policy",
+    `frame-ancestors 'self' ${FRONTEND_ORIGIN}`
+  );
+  res.setHeader("Content-Disposition", "inline");
+  next();
+});
+
+app.use("/uploads", express.static(path.join(__dirname, "..", "uploads")));
 
 app.use("/api", routes);
 setupSwagger(app);
