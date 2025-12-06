f105051acea0e1bfca1a63a5a5b17dcbf4407c1a
Matthieu Nzinga <matthieunzinga@gmail.com>
2025-12-06 09:21:59 +0100

    Add controller for paginated courriers of connected user

--- src/controllers/courrier.controller.js
+++ src/controllers/courrier.controller.js
@@ -111,4 +111,26 @@ exports.getCourriersPaginated = async (req, res) => {
   }
 };
 
+exports.getCourriersUserPaginated = async (req, res) => {
+  try {
+    const userId = req.user.userId;
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 10;
+
+    const data = await courrierService.findByUserPaginated(userId, page, limit);
+
+    const finalRows = data.rows.map(c => addPdfUrl(c, req));
+
+    res.json({
+      page: data.page,
+      limit: data.limit,
+      total: data.total,
+      totalPages: data.totalPages,
+      rows: finalRows
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Erreur serveur", err });
+  }
+};
 
